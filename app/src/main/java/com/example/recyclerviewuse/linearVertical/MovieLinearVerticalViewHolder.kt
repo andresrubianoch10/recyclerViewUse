b01deb2bec7415e1a9555529f3a86c6ebcf3fdc5
andres.rubiano <andres.rubiano@globant.com>
2022-05-24 19:13:43 -0500

    Added linear vertical view holder

---
+++ app/src/main/java/com/example/recyclerviewuse/linearVertical/MovieLinearVerticalViewHolder.kt
@@ -0,0 +1,25 @@
+package com.example.recyclerviewuse.linearVertical
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.recyclerviewuse.commons.Movie
+import com.example.recyclerviewuse.databinding.ItemViewHolderMovieLinearVerticalBinding
+import kotlin.math.roundToInt
+
+class MovieLinearVerticalViewHolder(
+    view: View
+) : RecyclerView.ViewHolder(view) {
+
+    private val binding = ItemViewHolderMovieLinearVerticalBinding.bind(view)
+
+    fun bind(model: Movie) {
+        with(binding) {
+            Glide.with(itemView).load(model.url).into(imageViewLinearVerticalPrincipal)
+            textViewLinearVerticalTitle.text = model.title
+            textViewLinearVerticalYear.text = model.year.toString()
+            textViewLinearVerticalType.text = model.type
+            ratingBarLinearVerticalRating.numStars = model.rating.roundToInt()
+        }
+    }
+}
