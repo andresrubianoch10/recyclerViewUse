0d563e0c86f013e0797c2e5c25f1af9289eeed1c
andres.rubiano <andres.rubiano@globant.com>
2022-05-24 19:33:40 -0500

    Added adapter and recyclew view

--- app/src/main/java/com/example/recyclerviewuse/linearVertical/LinearVerticalActivity.kt
+++ app/src/main/java/com/example/recyclerviewuse/linearVertical/LinearVerticalActivity.kt
@@ -2,11 +2,426 @@ package com.example.recyclerviewuse.linearVertical
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.recyclerviewuse.R
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.recyclerviewuse.commons.Movie
+import com.example.recyclerviewuse.databinding.ActivityLinearVerticalBinding
 
 class LinearVerticalActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLinearVerticalBinding
+
+    private lateinit var adapterMovies: MovieLinearVerticalAdapter
+
+    private val data = mutableListOf<Movie>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_linear_vertical)
+        binding = ActivityLinearVerticalBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        initRecyclerView()
+        initData()
+    }
+
+    private fun initRecyclerView() {
+        adapterMovies = MovieLinearVerticalAdapter(data)
+        with(binding.recyclerView) {
+            layoutManager = LinearLayoutManager(this@LinearVerticalActivity)
+            adapter = adapterMovies
+
+        }
+    }
+
+    private fun initData() {
+        val newData = getData()
+        data.clear()
+        data.addAll(newData)
+        adapterMovies.notifyDataSetChanged()
+    }
+
+    private fun getData(): List<Movie> {
+        return listOf(
+            Movie(
+                url = "https://www.themoviedb.org/t/p/w220_and_h330_face/iDah4ZcDX3F316LcLlmNNAzsRqF.jpg",
+                title = "Sully",
+                rating = 5.0F,
+                type = "HD",
+                year = 2020
+            ),
+            Movie(
+                url = "https://es.web.img3.acsta.net/c_310_420/pictures/14/05/28/11/24/435900.jpg",
+                title = "Cars",
+                rating = 3.5F,
+                type = "HD",
+                year = 2010
+            ),
+            Movie(
+                url = "https://www.themoviedb.org/t/p/w220_and_h330_face/iDah4ZcDX3F316LcLlmNNAzsRqF.jpg",
+                title = "Rapido y furioso",
+                rating = 1.0F,
+                type = "Full HD",
+                year = 2000
+            ),
+            Movie(
+                url = "https://www.themoviedb.org/t/p/w220_and_h330_face/iDah4ZcDX3F316LcLlmNNAzsRqF.jpg",
+                title = "Sully",
+                rating = 5.0F,
+                type = "HD",
+                year = 2020
+            ),
+            Movie(
+                url = "https://es.web.img3.acsta.net/c_310_420/pictures/14/05/28/11/24/435900.jpg",
+                title = "Cars",
+                rating = 3.5F,
+                type = "HD",
+                year = 2010
+            ),
+            Movie(
+                url = "https://www.themoviedb.org/t/p/w220_and_h330_face/iDah4ZcDX3F316LcLlmNNAzsRqF.jpg",
+                title = "Rapido y furioso",
+                rating = 1.0F,
+                type = "Full HD",
+                year = 2000
+            ),
+            Movie(
+                url = "https://www.themoviedb.org/t/p/w220_and_h330_face/iDah4ZcDX3F316LcLlmNNAzsRqF.jpg",
+                title = "Sully",
+                rating = 5.0F,
+                type = "HD",
+                year = 2020
+            ),
+            Movie(
+                url = "https://es.web.img3.acsta.net/c_310_420/pictures/14/05/28/11/24/435900.jpg",
+                title = "Cars",
+                rating = 3.5F,
+                type = "HD",
+                year = 2010
+            ),
+            Movie(
+                url = "https://www.themoviedb.org/t/p/w220_and_h330_face/iDah4ZcDX3F316LcLlmNNAzsRqF.jpg",
+                title = "Rapido y furioso",
+                rating = 1.0F,
+                type = "Full HD",
+                year = 2000
+            ),
+            Movie(
+                url = "https://www.themoviedb.org/t/p/w220_and_h330_face/iDah4ZcDX3F316LcLlmNNAzsRqF.jpg",
+                title = "Sully",
+                rating = 5.0F,
+                type = "HD",
+                year = 2020
+            ),
+            Movie(
+                url = "https://es.web.img3.acsta.net/c_310_420/pictures/14/05/28/11/24/435900.jpg",
+                title = "Cars",
+                rating = 3.5F,
+                type = "HD",
+                year = 2010
+            ),
+            Movie(
+                url = "https://www.themoviedb.org/t/p/w220_and_h330_face/iDah4ZcDX3F316LcLlmNNAzsRqF.jpg",
+                title = "Rapido y furioso",
+                rating = 1.0F,
+                type = "Full HD",
+                year = 2000
+            ),
+            Movie(
+                url = "https://www.themoviedb.org/t/p/w220_and_h330_face/iDah4ZcDX3F316LcLlmNNAzsRqF.jpg",
+                title = "Sully",
+                rating = 5.0F,
+                type = "HD",
+                year = 2020
+            ),
+            Movie(
+                url = "https://es.web.img3.acsta.net/c_310_420/pictures/14/05/28/11/24/435900.jpg",
+                title = "Cars",
+                rating = 3.5F,
+                type = "HD",
+                year = 2010
+            ),
+            Movie(
+                url = "https://www.themoviedb.org/t/p/w220_and_h330_face/iDah4ZcDX3F316LcLlmNNAzsRqF.jpg",
+                title = "Rapido y furioso",
+                rating = 1.0F,
+                type = "Full HD",
+                year = 2000
+            ),
+            Movie(
+                url = "https://www.themoviedb.org/t/p/w220_and_h330_face/iDah4ZcDX3F316LcLlmNNAzsRqF.jpg",
+                title = "Sully",
+                rating = 5.0F,
+                type = "HD",
+                year = 2020
+            ),
+            Movie(
+                url = "https://es.web.img3.acsta.net/c_310_420/pictures/14/05/28/11/24/435900.jpg",
+                title = "Cars",
+                rating = 3.5F,
+                type = "HD",
+                year = 2010
+            ),
+            Movie(
+                url = "https://www.themoviedb.org/t/p/w220_and_h330_face/iDah4ZcDX3F316LcLlmNNAzsRqF.jpg",
+                title = "Rapido y furioso",
+                rating = 1.0F,
+                type = "Full HD",
+                year = 2000
+            ),
+            Movie(
+                url = "https://www.themoviedb.org/t/p/w220_and_h330_face/iDah4ZcDX3F316LcLlmNNAzsRqF.jpg",
+                title = "Sully",
+                rating = 5.0F,
+                type = "HD",
+                year = 2020
+            ),
+            Movie(
+                url = "https://es.web.img3.acsta.net/c_310_420/pictures/14/05/28/11/24/435900.jpg",
+                title = "Cars",
+                rating = 3.5F,
+                type = "HD",
+                year = 2010
+            ),
+            Movie(
+                url = "https://www.themoviedb.org/t/p/w220_and_h330_face/iDah4ZcDX3F316LcLlmNNAzsRqF.jpg",
+                title = "Rapido y furioso",
+                rating = 1.0F,
+                type = "Full HD",
+                year = 2000
+            ),
+            Movie(
+                url = "https://www.themoviedb.org/t/p/w220_and_h330_face/iDah4ZcDX3F316LcLlmNNAzsRqF.jpg",
+                title = "Sully",
+                rating = 5.0F,
+                type = "HD",
+                year = 2020
+            ),
+            Movie(
+                url = "https://es.web.img3.acsta.net/c_310_420/pictures/14/05/28/11/24/435900.jpg",
+                title = "Cars",
+                rating = 3.5F,
+                type = "HD",
+                year = 2010
+            ),
+            Movie(
+                url = "https://www.themoviedb.org/t/p/w220_and_h330_face/iDah4ZcDX3F316LcLlmNNAzsRqF.jpg",
+                title = "Rapido y furioso",
+                rating = 1.0F,
+                type = "Full HD",
+                year = 2000
+            ),
+            Movie(
+                url = "https://www.themoviedb.org/t/p/w220_and_h330_face/iDah4ZcDX3F316LcLlmNNAzsRqF.jpg",
+                title = "Sully",
+                rating = 5.0F,
+                type = "HD",
+                year = 2020
+            ),
+            Movie(
+                url = "https://es.web.img3.acsta.net/c_310_420/pictures/14/05/28/11/24/435900.jpg",
+                title = "Cars",
+                rating = 3.5F,
+                type = "HD",
+                year = 2010
+            ),
+            Movie(
+                url = "https://www.themoviedb.org/t/p/w220_and_h330_face/iDah4ZcDX3F316LcLlmNNAzsRqF.jpg",
+                title = "Rapido y furioso",
+                rating = 1.0F,
+                type = "Full HD",
+                year = 2000
+            ),
+            Movie(
+                url = "https://www.themoviedb.org/t/p/w220_and_h330_face/iDah4ZcDX3F316LcLlmNNAzsRqF.jpg",
+                title = "Sully",
+                rating = 5.0F,
+                type = "HD",
+                year = 2020
+            ),
+            Movie(
+                url = "https://es.web.img3.acsta.net/c_310_420/pictures/14/05/28/11/24/435900.jpg",
+                title = "Cars",
+                rating = 3.5F,
+                type = "HD",
+                year = 2010
+            ),
+            Movie(
+                url = "https://www.themoviedb.org/t/p/w220_and_h330_face/iDah4ZcDX3F316LcLlmNNAzsRqF.jpg",
+                title = "Rapido y furioso",
+                rating = 1.0F,
+                type = "Full HD",
+                year = 2000
+            ),
+            Movie(
+                url = "https://www.themoviedb.org/t/p/w220_and_h330_face/iDah4ZcDX3F316LcLlmNNAzsRqF.jpg",
+                title = "Sully",
+                rating = 5.0F,
+                type = "HD",
+                year = 2020
+            ),
+            Movie(
+                url = "https://es.web.img3.acsta.net/c_310_420/pictures/14/05/28/11/24/435900.jpg",
+                title = "Cars",
+                rating = 3.5F,
+                type = "HD",
+                year = 2010
+            ),
+            Movie(
+                url = "https://www.themoviedb.org/t/p/w220_and_h330_face/iDah4ZcDX3F316LcLlmNNAzsRqF.jpg",
+                title = "Rapido y furioso",
+                rating = 1.0F,
+                type = "Full HD",
+                year = 2000
+            ),
+            Movie(
+                url = "https://www.themoviedb.org/t/p/w220_and_h330_face/iDah4ZcDX3F316LcLlmNNAzsRqF.jpg",
+                title = "Sully",
+                rating = 5.0F,
+                type = "HD",
+                year = 2020
+            ),
+            Movie(
+                url = "https://es.web.img3.acsta.net/c_310_420/pictures/14/05/28/11/24/435900.jpg",
+                title = "Cars",
+                rating = 3.5F,
+                type = "HD",
+                year = 2010
+            ),
+            Movie(
+                url = "https://www.themoviedb.org/t/p/w220_and_h330_face/iDah4ZcDX3F316LcLlmNNAzsRqF.jpg",
+                title = "Rapido y furioso",
+                rating = 1.0F,
+                type = "Full HD",
+                year = 2000
+            ),
+            Movie(
+                url = "https://www.themoviedb.org/t/p/w220_and_h330_face/iDah4ZcDX3F316LcLlmNNAzsRqF.jpg",
+                title = "Sully",
+                rating = 5.0F,
+                type = "HD",
+                year = 2020
+            ),
+            Movie(
+                url = "https://es.web.img3.acsta.net/c_310_420/pictures/14/05/28/11/24/435900.jpg",
+                title = "Cars",
+                rating = 3.5F,
+                type = "HD",
+                year = 2010
+            ),
+            Movie(
+                url = "https://www.themoviedb.org/t/p/w220_and_h330_face/iDah4ZcDX3F316LcLlmNNAzsRqF.jpg",
+                title = "Rapido y furioso",
+                rating = 1.0F,
+                type = "Full HD",
+                year = 2000
+            ),
+            Movie(
+                url = "https://www.themoviedb.org/t/p/w220_and_h330_face/iDah4ZcDX3F316LcLlmNNAzsRqF.jpg",
+                title = "Sully",
+                rating = 5.0F,
+                type = "HD",
+                year = 2020
+            ),
+            Movie(
+                url = "https://es.web.img3.acsta.net/c_310_420/pictures/14/05/28/11/24/435900.jpg",
+                title = "Cars",
+                rating = 3.5F,
+                type = "HD",
+                year = 2010
+            ),
+            Movie(
+                url = "https://www.themoviedb.org/t/p/w220_and_h330_face/iDah4ZcDX3F316LcLlmNNAzsRqF.jpg",
+                title = "Rapido y furioso",
+                rating = 1.0F,
+                type = "Full HD",
+                year = 2000
+            ),
+            Movie(
+                url = "https://www.themoviedb.org/t/p/w220_and_h330_face/iDah4ZcDX3F316LcLlmNNAzsRqF.jpg",
+                title = "Sully",
+                rating = 5.0F,
+                type = "HD",
+                year = 2020
+            ),
+            Movie(
+                url = "https://es.web.img3.acsta.net/c_310_420/pictures/14/05/28/11/24/435900.jpg",
+                title = "Cars",
+                rating = 3.5F,
+                type = "HD",
+                year = 2010
+            ),
+            Movie(
+                url = "https://www.themoviedb.org/t/p/w220_and_h330_face/iDah4ZcDX3F316LcLlmNNAzsRqF.jpg",
+                title = "Rapido y furioso",
+                rating = 1.0F,
+                type = "Full HD",
+                year = 2000
+            ),
+            Movie(
+                url = "https://www.themoviedb.org/t/p/w220_and_h330_face/iDah4ZcDX3F316LcLlmNNAzsRqF.jpg",
+                title = "Sully",
+                rating = 5.0F,
+                type = "HD",
+                year = 2020
+            ),
+            Movie(
+                url = "https://es.web.img3.acsta.net/c_310_420/pictures/14/05/28/11/24/435900.jpg",
+                title = "Cars",
+                rating = 3.5F,
+                type = "HD",
+                year = 2010
+            ),
+            Movie(
+                url = "https://www.themoviedb.org/t/p/w220_and_h330_face/iDah4ZcDX3F316LcLlmNNAzsRqF.jpg",
+                title = "Rapido y furioso",
+                rating = 1.0F,
+                type = "Full HD",
+                year = 2000
+            ),
+            Movie(
+                url = "https://www.themoviedb.org/t/p/w220_and_h330_face/iDah4ZcDX3F316LcLlmNNAzsRqF.jpg",
+                title = "Sully",
+                rating = 5.0F,
+                type = "HD",
+                year = 2020
+            ),
+            Movie(
+                url = "https://es.web.img3.acsta.net/c_310_420/pictures/14/05/28/11/24/435900.jpg",
+                title = "Cars",
+                rating = 3.5F,
+                type = "HD",
+                year = 2010
+            ),
+            Movie(
+                url = "https://www.themoviedb.org/t/p/w220_and_h330_face/iDah4ZcDX3F316LcLlmNNAzsRqF.jpg",
+                title = "Rapido y furioso",
+                rating = 1.0F,
+                type = "Full HD",
+                year = 2000
+            ),
+            Movie(
+                url = "https://www.themoviedb.org/t/p/w220_and_h330_face/iDah4ZcDX3F316LcLlmNNAzsRqF.jpg",
+                title = "Sully",
+                rating = 5.0F,
+                type = "HD",
+                year = 2020
+            ),
+            Movie(
+                url = "https://es.web.img3.acsta.net/c_310_420/pictures/14/05/28/11/24/435900.jpg",
+                title = "Cars",
+                rating = 3.5F,
+                type = "HD",
+                year = 2010
+            ),
+            Movie(
+                url = "https://www.themoviedb.org/t/p/w220_and_h330_face/iDah4ZcDX3F316LcLlmNNAzsRqF.jpg",
+                title = "Rapido y furioso",
+                rating = 1.0F,
+                type = "Full HD",
+                year = 2000
+            )
+        )
     }
 }
---
+++ app/src/main/java/com/example/recyclerviewuse/linearVertical/MovieLinearVerticalAdapter.kt
@@ -0,0 +1,34 @@
+package com.example.recyclerviewuse.linearVertical
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.recyclerviewuse.R
+import com.example.recyclerviewuse.commons.Movie
+
+class MovieLinearVerticalAdapter(
+    val data: List<Movie>
+) : RecyclerView.Adapter<MovieLinearVerticalViewHolder>() {
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): MovieLinearVerticalViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        return MovieLinearVerticalViewHolder(
+            layoutInflater.inflate(
+                R.layout.item_view_holder_movie_linear_vertical,
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: MovieLinearVerticalViewHolder, position: Int) {
+        val item = data[position]
+        holder.bind(item)
+    }
+
+    override fun getItemCount(): Int = data.size
+
+}
